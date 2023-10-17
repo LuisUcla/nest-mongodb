c4ef4d4603670298042407aaff35bddd8c9f1f70
LuisUcla <caluigi7@gmail.com>
2023-10-17 10:42:25 -0400

    refactor: change mongooseModule forFeature name

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BookSchema } from './schemas/book.schema';
+import { Book, BookSchema } from './schemas/book.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Book', schema: BookSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])],
   controllers: [BookController],
   providers: [BookService]
 })
