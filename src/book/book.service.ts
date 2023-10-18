83c08ec6f0cf8796c218435e40dabf4cd6eb0f3a
LuisUcla <caluigi7@gmail.com>
2023-10-18 17:59:04 -0400

    refactor: change method of pagination and validation of create new book

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -5,6 +5,7 @@ import * as mongoose from 'mongoose';
 import { CreateBookDto } from './dto/create-book.dto';
 
 import { Query } from 'express-serve-static-core'
+import { User } from '../auth/schemas/user.schema';
 
 @Injectable()
 export class BookService {
@@ -13,26 +14,27 @@ export class BookService {
         private bookModel: mongoose.Model<Book>
     ) {}
 
-    async createBook(book: CreateBookDto): Promise<Book> {
+    async createBook(book: CreateBookDto, user: User): Promise<Book> {
 
         const bookAlreadyExists = await this.bookModel.findOne({
             title: book.title,
             author: book.author,
             category: book.category
         })
-        console.log(bookAlreadyExists)
 
         if (bookAlreadyExists) {
-            throw new ConflictException('Book already exists...')
+            throw new ConflictException('The book already exists with that author and category....')
         }
 
-        const bookRes = await this.bookModel.create(book);
+        const data = { ...book, user: user._id }
+
+        const bookRes = await this.bookModel.create(data);
         return bookRes;
     }
 
-    async findBooksFilterByKeyword(query: Query) {
+    async findBooksFilterByKeyword(query: Query) { // se usa para busqueda por titulo
 
-        const resPerPage = 3
+        const resPerPage = Number(query.resPerPage) || 1
         const currentPage = Number(query.page) || 1
         const skip = resPerPage * (currentPage - 1) // --> paginacion
 
@@ -49,17 +51,28 @@ export class BookService {
             .find({ ...keyword })
             .limit(resPerPage) // Especifica el número máximo de documentos que devolverá la consulta.
             .skip(skip); // skip: Especifica el número de documentos que se omitirán.
+
+            // logic
+            const numberPages = resPerPage !== 0 ? Math.ceil(total.length / resPerPage) : 5;
+            const prev = currentPage !== 1 && books.length ? true : false;
+            const next = currentPage !== numberPages && books.length ? true : false;
         
         return {
             currentPage,
             elements: books.length,
             total: total.length,
-            books: [...books],
+            prev,
+            numberPages,
+            next,
+            books
         };
     }
 
     async findAll(): Promise <Book[]> {
-        const books = await this.bookModel.find();
+        const books = await this.bookModel
+            .find()
+            .populate('user', ['name', 'email'],);
+
         return books;
     }
 
@@ -71,7 +84,9 @@ export class BookService {
             throw new BadRequestException('Id is invalid, enter Id Correct...')
         }
         
-        const book = await this.bookModel.findById(id);
+        const book = await this.bookModel
+            .findById(id)
+            .populate('user', ['name', 'email'],);
         
         if (!book) {
             throw new NotFoundException('Book Not Found...')
